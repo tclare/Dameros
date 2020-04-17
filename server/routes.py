0d80840267f975a64dd44e4c75aa0ec2f9cb2243
Jack <jrundle@nd.edu>
2020-04-16 19:41:45 -0500

    page routing

--- server/routes.py
+++ server/routes.py
@@ -1,7 +1,13 @@
 from server import app
 from flask import render_template
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html")
+
+
+@app.route('/apply')
+def apply_func():
+    return render_template("apply.html")
