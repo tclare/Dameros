cbc7f90e2b1bd148cb3103a4b8dbd3afb2b2302a
tclare <tclare@nd.edu>
2020-04-23 18:54:23 -0400

    updates to success stories

---
+++ server/static/js/index.js
@@ -0,0 +1,13 @@
+
+/*$(document).ready(function() {
+ 
+ computeSuccessRowMargin();
+ $(window).resize(computeSuccessRowMargin);
+});
+
+function computeSuccessRowMargin() { 
+    if (window.innerWidth < 576) {
+        var new_value = 576 - window.innerWidth + 400 + "px";
+        $('.success-row').css('margin-top',  new_value + "px");
+    }
+}*/
