caa0737e625b912ef58f3fb63ceeb92815a7fc19
Andrew Slavin <andrewslavin@Andrews-MacBook-Air.local>
2020-04-23 21:51:12 -0700

    Andy added his silly game

--- server/routes.py
+++ server/routes.py
@@ -15,3 +15,7 @@ def apply_func():
 @app.route('/success_stories')
 def success_stories_func():
     return render_template("success_stories.html")
+
+@app.route('/tilt_a_roll')
+def tilt_a_roll_func():
+    return render_template("play.html")
