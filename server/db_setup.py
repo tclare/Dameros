d2457e25d5881eb578b72d27a14bdeed6724a687
Andrew Slavin <andrewslavin@andrews-air.lan>
2020-05-24 14:20:24 -0400

    added database setup and teardown

---
+++ server/db_setup.py
@@ -0,0 +1,81 @@
+import psycopg2
+import os
+from server import app
+
+def drop_dynamic_content():
+    db_url = app.config["DATABASE_URL"]
+    
+    conn = psycopg2.connect(db_url, sslmode="require")
+
+    query = "DROP TABLE dynamic_content"
+
+    cursor = conn.cursor()
+    cursor.execute(query)
+    cursor.close()
+    conn.commit()
+    conn.close()
+
+    return "It worked"
+
+def drop_form_entries():
+    db_url = app.config["DATABASE_URL"]
+    
+    conn = psycopg2.connect(db_url, sslmode="require")
+
+    query = "DROP TABLE form_responses"
+
+    cursor = conn.cursor()
+    cursor.execute(query)
+    cursor.close()
+    conn.commit()
+    conn.close()
+
+    return "It worked"
+
+def create_dynamic_content_table():
+    db_url = app.config["DATABASE_URL"]
+    
+    conn = psycopg2.connect(db_url, sslmode="require")
+    
+    query = """
+        CREATE TABLE dynamic_content(
+            element_id VARCHAR() PRIMARY KEY,
+            page_id VARCHAR(),
+            content VARCHAR()
+        )
+    """
+
+    cursor = conn.cursor()
+    cursor.execute(query)
+    cursor.close()
+    conn.commit()
+    conn.close()
+
+    return "It worked"
+
+def create_form_responses_table():
+    db_url = app.config["DATABASE_URL"]
+    
+    conn = psycopg2.connect(db_url, sslmode="require")
+
+    query = """
+            CREATE TABLE form_responses(
+                id SERIAL PRIMARY KEY,
+                first_name VARCHAR() NOT NULL,
+                last_name VARCHAR() NOT NULL,
+                email VARCHAR(),
+                sport VARCHAR(),
+                agent_email VARCHAR(),
+                agent_phone VARCHAR(),
+                time TIMESTAMP NOT NULL, 
+                interest VARCHAR()
+            )
+        """
+
+    cursor = conn.cursor()
+    cursor.execute(query)
+    cursor.close()
+    conn.commit()
+    conn.close()
+
+    return "It worked"
--- server/routes.py
+++ server/routes.py
@@ -1,13 +1,13 @@
 from server import app
 from flask import render_template
 from server import database
+from server import db_setup
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html")
-
 
 @app.route('/apply')
 def apply_func():
@@ -17,10 +17,18 @@ def apply_func():
 def success_stories_func():
     return render_template("success_stories.html")
 
+@app.route('/admin')
+def admin_func():
+    return render_template("admin.html")
+
+@app.route('/destroy_old_form_entries')
+def destroy_old_form_entries():
+    result = db_setup.drop_form_entries()
+    return result
+
 @app.route('/tilt_a_roll')
 def tilt_a_roll_func():
     return render_template("play.html")
-
 
 @app.route('/test')
 def test_func():
