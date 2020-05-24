d51719282fb962e0fc9712f55ceddd4bfd7c55e7
tclare <tclare@nd.edu>
2020-05-23 23:49:57 -0400

    admin changes 5/23: textarea onchange works; image upload works; new routes for each

--- server/routes.py
+++ server/routes.py
@@ -1,7 +1,6 @@
 from server import app
-from flask import render_template
+from flask import render_template, jsonify, request
 from server import database
-
 
 @app.route('/')
 @app.route('/index')
@@ -21,6 +20,27 @@ def success_stories_func():
 def tilt_a_roll_func():
     return render_template("play.html")
 
+@app.route('/admin')
+def admin_func():
+    return render_template("admin.html")
+
+@app.route('/text_content', methods=['PUT'])
+def text_content():
+    ## TODO: Carefully check authentication (of Lauren) via session variables
+    ### How to grab id and value that were just changed (error handle them): 
+    ## id, value = request.json["id"], request.json["value"])
+    ## TODO: Database update operation with these values that changes website content
+    return jsonify({'success': 'yes'})
+
+@app.route('/image_content', methods=['POST'])
+def image_content():
+    ## TODO: Carefully check authentication (of Lauren) via session variables
+    ### How to grab image file that was just changed (error handle / make sure it has some content):
+    id = dict(request.files).keys()[0] ## ex. 'success-stories-image-1'. Id representing image file to change.
+    data = request.files[id] ## ex. <FileStorage: 'fullsizeoutput_1f3.jpeg' ('image/jpeg')>. Contents of uploaded file.
+    ## use github api ?? To upload this new file ??  
+    ## TODO: git add, commit, push
+    return jsonify({'success': 'yes'})
 
 @app.route('/test')
 def test_func():
--- server/static/js/index.js
+++ server/static/js/index.js
@@ -1,22 +1,35 @@
 $(document).ready(function() {
-  $("textarea").change(inputChanged);
+  $("textarea").change(textInputChanged); // all text inputs are <textarea>s
+  $("input").change(imageInputChanged);  // all image inputs are <input>s
+  
 });
 
-function inputChanged(){
+// FUNCTION (utility): protocol_hostname_port
+// params: none
+// returns string: protocol, hostname, port from current URL. Avoids hardcoding in API calls.
+function protocol_hostname_port() { 
+  return location.protocol + '//' + location.hostname + (location.port ? ':'+location.port: '');
+}
+
+function file_chosen(form_data) { 
+
+}
+
+function textInputChanged(){
   console.log("Here");
   // var v = document.getElementById("first-landing-paragraph").value; <== normal JS
   // var v = $('#first-landing-paragraph') // <== jquery
   //console.log($(this));
   var admin_id = $(this).attr('id');
   var admin_val = $(this).val();
-  var o = {};
+  var o = {id: admin_id, value: admin_val};
   o[admin_id] = admin_val;
-  console.log(o);
-/*
+
   // Make a request to the server.
   var xhttp = new XMLHttpRequest();
   // Step 1: send request to server.
-  xhttp.open("PUT", "http://dameros.herokuapp.com/put_content/", true); // GET, POST (add something new), PUT (modify something), DELETE (get rid of something),
+  xhttp.open("PUT", protocol_hostname_port() + "/text_content", true); // GET, POST (add something new), PUT (modify something), DELETE (get rid of something),
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
   xhttp.send(JSON.stringify(o));
   xhttp.onreadystatechange = function (e) {
     if (xhttp.readyState == 4) {
@@ -28,7 +41,7 @@ function inputChanged(){
       // {"success": "yes"}
       // {"success": "no", "error": "error message asdf asdf asdf"}
     }
-  }*/
+  }
 
 
   /* JSON example: {
@@ -38,4 +51,20 @@ function inputChanged(){
     ]
   }*/
 
+}
+
+
+function imageInputChanged() {
+  var admin_id = $(this).attr('id');
+  var form_data = new FormData();
+  form_data.append(admin_id, $('#' + admin_id)[0].files[0]);
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("POST", protocol_hostname_port() + "/image_content", true);
+  xhttp.send(form_data);
+  xhttp.onreadystatechange = function (e) {
+    if (xhttp.readyState == 4) {
+      var response = xhttp.responseText;
+      console.log(response);
+    }
+  }
 }
